511683839a3600a61b0abfaf3142df833057b33b
Wes Dean <wesley-dean@users.noreply.github.com>
2026-09-23 17:06:07 -0400

    fix: make transport arrays minifier-safe

--- letsencrypt-routeros.bash
+++ letsencrypt-routeros.bash
@@ -1386,21 +1386,8 @@ build_transport_commands() {
     read -r -a extra_options <<< "${ROUTEROS_SSH_OPTIONS}"
   fi
 
-  routeros_ssh=(
-    ssh
-    -i "${ROUTEROS_PRIVATE_KEY}"
-    -p "${ROUTEROS_SSH_PORT}"
-    "${extra_options[@]}"
-    "${ROUTEROS_USER}@${ROUTEROS_HOST}"
-  )
-
-  routeros_scp=(
-    scp
-    -q
-    -P "${ROUTEROS_SSH_PORT}"
-    -i "${ROUTEROS_PRIVATE_KEY}"
-    "${extra_options[@]}"
-  )
+  routeros_ssh=(ssh -i "${ROUTEROS_PRIVATE_KEY}" -p "${ROUTEROS_SSH_PORT}" "${extra_options[@]}" "${ROUTEROS_USER}@${ROUTEROS_HOST}")
+  routeros_scp=(scp -q -P "${ROUTEROS_SSH_PORT}" -i "${ROUTEROS_PRIVATE_KEY}" "${extra_options[@]}")
 }
 
 verify_connection() {
